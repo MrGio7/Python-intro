0c947410a5dc46bcff3bc770dbf4b6501c3dac36
MrGio7 <lashagiorgi1233@gmail.com>
2020-02-28 15:10:44 +0400

    added loops

--- src/intro.py
+++ src/intro.py
@@ -14,3 +14,21 @@ print(li)
 #Loop
 for number in li:
     print(number)
+
+for (i, e) in enumerate(li):
+    print(f'Element at {i} is {e}')
+
+squares = [num*num for num in li]
+
+#for sqr in li:
+#    squares.append(sqr*sqr)
+
+print(squares)
+
+evens = [num for num in li if num % 2 == 0]
+
+#for num in li:
+#    if num % 2 == 0:
+#        evens.append(num)
+
+print(evens)
