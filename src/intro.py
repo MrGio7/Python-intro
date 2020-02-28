207fcff02e49db14c8467d833b6faedb1758f6d9
MrGio7 <lashagiorgi1233@gmail.com>
2020-02-28 13:46:40 +0400

    created pyton little guide

---
+++ src/intro.py
@@ -0,0 +1,12 @@
+print ("Hello World")
+
+# Declaring Variables
+name = "Gio"
+print (name)
+print ("Hello " + name)
+print (f'Hello {name}')
+
+#Working with List
+li = [12, 5, 65, 15]
+li.append(11)
+print(li) 
